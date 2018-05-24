576ec7cc52a98b9724851c1c73b667b41e9173d1
22ndtech <jeff@22ndtech>
2018-05-23 20:01:20 -0600

    Removed Resumable1

--- Resumable1.cpp
+++
@@ -1,9 +0,0 @@
-#include "Resumable1.h"
-
-
-
-
-
-bool Resumable1::awaiter::await_ready() const noexcept {
-    return m_resumable1.isSet();
-}
--- Resumable1.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <experimental\coroutine>
-#include <iostream>
-#include <string>
-
-using namespace std;
-using namespace std::experimental;
-
